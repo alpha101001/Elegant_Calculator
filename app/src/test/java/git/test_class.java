5f28072dcff05272ee51b0ed2fa3590123974d48
Avishek Roy <roy1807053@stud.kuet.ac.bd>
2022-05-18 00:59:40 +0600

    java class added

---
+++ app/src/test/java/git/test_class.java
@@ -0,0 +1,28 @@
+package git;
+
+public class test_class {
+    int roll;
+    String name;
+
+    test_class(String a, int b){
+        this.roll=a;
+        this.name=b;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
